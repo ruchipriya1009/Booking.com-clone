27ba839bef733f69a65c0da7deb59df95c951a7f
Abhra11 <abhra001597@gmail.com>
2022-12-14 19:06:06 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from "./pages/home/Home";
-import Hotel from "./pages/hotel/Hotel";
-import List from "./pages/list/List";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Flights from "./FlightPage/FlightMain";
+// import Home from "./pages/home/Home";
+// import Hotel from "./pages/hotel/Hotel";
+// import List from "./pages/list/List";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        {/* <Route path="/" element={<Home/>} />
         <Route path="/hotels" element={<List/>} />
-        <Route path="/hotels/:id" element={<Hotel/>} />
+        <Route path="/hotels/:id" element={<Hotel/>} /> */}
+        <Route path="/flights" element={<Flights />} />
       </Routes>
     </BrowserRouter>
   );
