a06cc8c96cced786f24ef5ecfd4477ea14b684b5
Prashant_Rao <prashantraogunjal1562@gmail.com>
2022-12-14 22:32:58 +0530

    day-2

---
+++ src/actions/attractionAction.js
@@ -0,0 +1,19 @@
+import { FETCHING, FETCH_SUCCESS, FETCH_FALIURE } from "./attractionActionType"
+
+export  const Fetching=()=>{
+    return{
+        type:FETCHING
+    }
+}
+export  const FetchSuccess=(payload)=>{
+    return{
+        type:FETCH_SUCCESS,
+        payload:payload
+    }
+}
+export const FetchFaliure=(error)=>{
+    return{
+        type:FETCH_FALIURE,
+        error:error
+    }
+}
