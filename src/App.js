4adc88118f55d9be5f8373d85a0cab80e439624a
Prashant_Rao <prashantraogunjal1562@gmail.com>
2022-12-15 23:10:25 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import AllRoutes from './Routes/AllRoutes';
 import {Navbar} from "./Navbar/Navbar";
  // import Routes from './Routes/AllRoutes'
 import Footer from './Footer/Footer';
-import AttractionIndex from "./Attractions/AttractionIndex";
+import AttractionIndex from "./components/Attractions/AttractionIndex";
 
 function App() {
   return (
